66de62c5e545e7bbcbb5ed17f1c36addaa9f524c
Fernanda <fernanda19@usf.edu>
2024-09-13 22:56:05 -0400

    Task 2: Create displayProductDetails function

--- inventory_management.js
+++ inventory_management.js
@@ -4,25 +4,37 @@ let inventory = [
         price: 5,
         quantity: 200,
         lowStockLevel: 100,
-    }, 
+    }, // Product 1, in stock
     { name: "popcorn",
         price: 10,
         quantity: 300,
         lowStockLevel: 70,
-    }, 
+    }, // product 2, in stock
     { name: "radio",
         price: 50,
         quantity: 100,
         lowStockLevel: 20,
-    }, 
+    }, // product 3, in stock
     { name: "hat",
 price: 15,
 quantity: 20,
 lowStockLevel: 50,
-    }, 
+    }, // product 4, low stock
     {name: "lipstick",
         price: 30,
         quantity: 100,
 lowStockLevel: 150
-    }
+    } // product 5, low stock
 ]; 
+
+// Create a Function to Display Product Details
+
+function displayProductDetails (inventory) {
+    let stockOfProducts = inventory.lowStockLevel >= inventory.quantity ? "Low Stock": "In Stock"
+// ternary operator because it was easier to define a name 'stockOfProducts' to use later 
+    console.log(` ${inventory.name} is priced at $${inventory.price} with current quantity of ${inventory.quantity} which means it is ${stockOfProducts} `); // created the phrase I want it to log as 
+return displayProductDetails; // returns function
+};
+
+inventory.forEach(inventory => displayProductDetails(inventory)) // uses forEach function to iterate over the array and log the products in the phrase prepared in the function. Expected output example: Lipstick is priced at $30 with current quantity of 100 which means it is Low Stock
+    
