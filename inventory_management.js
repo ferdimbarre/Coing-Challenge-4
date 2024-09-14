475f9a38deca3d9041be9eb8ef9102f3d8e4193c
Fernanda <fernanda19@usf.edu>
2024-09-13 22:26:40 -0400

    Task 1: Initialize inventory with product objects

---
+++ inventory_management.js
@@ -0,0 +1,28 @@
+// Inventory Array of Product Objects
+let inventory = [
+    { name: "candy",
+        price: 5,
+        quantity: 200,
+        lowStockLevel: 100,
+    }, 
+    { name: "popcorn",
+        price: 10,
+        quantity: 300,
+        lowStockLevel: 70,
+    }, 
+    { name: "radio",
+        price: 50,
+        quantity: 100,
+        lowStockLevel: 20,
+    }, 
+    { name: "hat",
+price: 15,
+quantity: 20,
+lowStockLevel: 50,
+    }, 
+    {name: "lipstick",
+        price: 30,
+        quantity: 100,
+lowStockLevel: 150
+    }
+]; 
